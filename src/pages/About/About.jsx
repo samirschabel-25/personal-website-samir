74bb369611d81cf085b9690974fe9fe32a0c1dc4
“Samir <schabel.samir@gmail.com>
2025-12-19 13:47:16 +0100

    changing navigation elements into react-router element in different components

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,6 +4,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Card from "react-bootstrap/Card";
+import { Link, NavLink } from 'react-router-dom';
 function About() {
   return (
    <>  
@@ -13,10 +14,10 @@ function About() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>
-                <Nav.Link href="/cv">Curriculum Vitae</Nav.Link>
-            </Nav>
+              <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+              <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+              <Nav.Link as={NavLink} to="/cv">Curriculum Vitae</Nav.Link>
+          </Nav>
             </Navbar.Collapse>
         </Container>
     </Navbar>
--- src/pages/CV/CurriculumVitae.jsx
+++ src/pages/CV/CurriculumVitae.jsx
@@ -7,6 +7,7 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import Cards from '../../components/Cards';
+import { Link, NavLink } from 'react-router-dom';
 
 function CurriculumVitae() {
 
@@ -20,10 +21,10 @@ function CurriculumVitae() {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-                <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/about">About</Nav.Link>
-                <Nav.Link href="/cv">Curriculum Vitae</Nav.Link>
-            </Nav>
+              <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+              <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+              <Nav.Link as={NavLink} to="/cv">Curriculum Vitae</Nav.Link>
+          </Nav>
             </Navbar.Collapse>
         </Container>
     </Navbar>
